f79c1e97964943edef9ffd09407a4db257538fc1
isomorphica <chevalier.jared@gmail.com>
2017-11-08 11:40:21 -0500

    growth calculation, script creation
    a number of central scripts created + empty.
    GameManager work begun. (modeled temporarily in MonoBehaviour
    CanvasManager using public variables)

---
+++ Assets/Scripts/GV.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GV {
+
+    public static float SunFactor(float sun)   // arg range: [0, 1];  return range: [0, 1]
+    {
+        return Mathf.Clamp((27f / 25f) * Mathf.Pow(Mathf.Clamp(sun, 0, 1), 2) - 0.08f, 0, 1);
+    }
+    public static float AirFactor(float air)
+    {
+        return Mathf.Clamp(-(Mathf.Pow((Mathf.Clamp(air, 0, 1) - 1), 2)) + 1, 0, 1);
+    }
+    public static float WaterFactor(float water)
+    {
+        water = Mathf.Clamp(water, 0, 1);
+        if (water < 1f / 3f)
+            return -0.5f * Mathf.Cos(water * 3 * Mathf.PI) + 0.5f;
+        else if (water > 2f / 3f)
+            return -0.5f * Mathf.Cos(water * 3 * Mathf.PI - 3) + 0.5f;
+        else return 1;
+    }
+}
---
+++ Assets/Scripts/UI/CanvasManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+    public float sun = 0;
+    public float air = 0;
+    public float water = 0;
+    public float sunFactor = 0;
+    public float airFactor = 0;
+    public float waterFactor = 0;
+    public float growth = 0;
+    public float height = 0;
+    public float time = 0;
+    public Transform growthMeter;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        UpdateGrowth();
+        height += growth * Time.deltaTime / 10;
+        water -= Time.deltaTime * (growth / 10 + sun) / 50;
+        if (water < 0)
+            water = 0;
+        waterFactor = GV.WaterFactor(water);
+    }
+    public void UpdateGrowth()
+    {
+        sunFactor = GV.SunFactor(sun);
+        airFactor = GV.AirFactor(air);
+        waterFactor = GV.WaterFactor(water);
+        growth = sunFactor * airFactor * waterFactor * 10;
+        growthMeter.position = new Vector3(growthMeter.position.x, growth/2f-4f, growthMeter.position.z);
+
+    }
+}
