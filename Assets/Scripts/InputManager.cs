dd413fefa21bd70752a61dc036da1e0918fa2786
mattstg <mattstger@gmail.com>
2017-11-08 02:16:56 -0500

    Folders

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    
+	public Vector2 clickableSize = new Vector2(.5f,.5f);
+
+
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _MouseClicked(Input.mousePosition);
+        }
+        else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            _MouseClicked(Input.GetTouch(0).position);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            MouseClickedOnObjOfInterest();
+    }
+	public void OnMouseOver()
+	{
+		MouseOver (Camera.main.ScreenToWorldPoint(Input.mousePosition));
+	}
+
+	private void _MouseClicked(Vector2 clickPos)
+	{
+		Vector2 clickedPos = Camera.main.ScreenToWorldPoint (clickPos);
+		MouseDown (clickedPos);
+	}
+
+	protected virtual void MouseOver(Vector2 mouseWorldPos)
+	{
+
+	}
+
+	protected virtual void MouseDown(Vector2 mouseWorldPos)
+	{
+
+	}
+
+	protected virtual void MouseClickedOnObjOfInterest()
+	{
+
+	}
+}
---
+++ Assets/Scripts/PlayerTouchControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTouchControls : InputManager {
+
+    protected override void MouseOver(Vector2 mouseWorldPos)
+    {
+
+    }
+
+    protected override void MouseDown(Vector2 mouseWorldPos)
+    {
+
+    }
+    
+}
