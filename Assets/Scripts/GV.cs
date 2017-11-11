0dd0f3127b796bf69469299a1a153d4944e90fbb
isomorphica <chevalier.jared@gmail.com>
2017-11-11 05:00:20 -0500

    metertest scene, shadowtest scene

--- Assets/Scripts/GV.cs
+++ Assets/Scripts/GV.cs
@@ -6,33 +6,32 @@ public class GV {
 
     public static float SunFactor(float sun)   // arg range: [0, 1];  return range: [0, 1]
     {
-        return Mathf.Clamp((27f / 25f) * Mathf.Pow(Mathf.Clamp(sun, 0, 1), 2) - 0.08f, 0, 1);
+        if (sun <= (11f / 60f))
+            return 0;
+        else
+            return Mathf.Clamp(0.5f * Mathf.Sin(1.2f * Mathf.PI * (Mathf.Clamp(sun, 0, 1) - 0.6f)) + 0.5f, 0, 1);
+        //return Mathf.Clamp((27f / 25f) * Mathf.Pow(Mathf.Clamp(sun, 0, 1), 2) - 0.08f, 0, 1);
     }
+
     public static float AirFactor(float air)
     {
         return Mathf.Clamp(-(Mathf.Pow((Mathf.Clamp(air, 0, 1) - 1), 2)) + 1, 0, 1);
     }
+
     public static float WaterFactor(float water)
     {
         water = Mathf.Clamp(water, 0, 1);
-        if (water < 1f / 3f)
-            return -0.5f * Mathf.Cos(water * 3 * Mathf.PI) + 0.5f;
-        else if (water > 2f / 3f)
-            return -0.5f * Mathf.Cos(water * 3 * Mathf.PI - 3) + 0.5f;
-        else return 1;
+        return 4 * water - 4 * Mathf.Pow(water, 2);
+        //return Mathf.Clamp(Mathf.Sqrt(1 - Mathf.Pow(2 * water - 1, 2)), 0, 1);
     }
 
     public static float GetMinimalRotation(float source, float target)
     {
         float delta = target - source;
         while (delta > 180)
-        {
             delta -= 360;
-        }
         while (delta < -180)
-        {
             delta += 360;
-        }
         return delta;
     }
 }
---
+++ Assets/Scripts/Gameplay/MeterTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeterTest : MonoBehaviour
+{
+
+    public float sun = 0;
+    public float air = 0;
+    public float water = 0;
+    public float sunFactor = 0;
+    public float airFactor = 0;
+    public float waterFactor = 0;
+    public float growth = 0;
+    public float growthDamp = 0;
+    public float height = 0;
+    public float time = 0;
+    public float dampTime = 0.5f;
+    private float velocity = 0;
+    public Transform growthMeter;
+    public Transform heightMeter;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        UpdateGrowth();
+        height += growth * Time.deltaTime / 10;
+        heightMeter.eulerAngles = new Vector3(heightMeter.eulerAngles.x, heightMeter.eulerAngles.y, height * -360 - 90);
+        water -= Time.deltaTime * (growth / 10 + sun) / 100;
+        if (water < 0)
+            water = 0;
+        waterFactor = GV.WaterFactor(water);
+    }
+    public void UpdateGrowth()
+    {
+        sunFactor = GV.SunFactor(sun);
+        airFactor = GV.AirFactor(air);
+        waterFactor = GV.WaterFactor(water);
+        growth = sunFactor * airFactor * waterFactor * 10;
+        growthDamp = Mathf.SmoothDamp(growthDamp, growth, ref velocity, dampTime);
+        float meterAngle = 90 - 180 * (growthDamp / 10);
+        growthMeter.eulerAngles = new Vector3(growthMeter.eulerAngles.x, growthMeter.eulerAngles.y, meterAngle);
+
+    }
+}
