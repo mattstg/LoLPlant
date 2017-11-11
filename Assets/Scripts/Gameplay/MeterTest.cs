ab291de52d77b818696a6cdcb2e7a2102726a2dd
isomorphica <chevalier.jared@gmail.com>
2017-11-11 07:53:25 -0500

    metertest

--- Assets/Scripts/Gameplay/MeterTest.cs
+++ Assets/Scripts/Gameplay/MeterTest.cs
@@ -20,6 +20,7 @@ public class MeterTest : MonoBehaviour
     private float velocity = 0;
     public Transform growthMeter;
     public Transform heightMeter;
+    public TextMesh heightText;
 
     void Update()
     {
@@ -27,6 +28,7 @@ public class MeterTest : MonoBehaviour
         UpdateGrowth();
         height += growth * Time.deltaTime / 10;
         heightMeter.eulerAngles = new Vector3(heightMeter.eulerAngles.x, heightMeter.eulerAngles.y, height * -360 - 90);
+        heightText.text = ((float)((int)(height * 10f))/10f).ToString("0.0") + "\n<size=24>cm</size>";
         water -= Time.deltaTime * (growth / 10 + sun) / 100;
         if (water < 0)
             water = 0;
@@ -39,7 +41,7 @@ public class MeterTest : MonoBehaviour
         waterFactor = GV.WaterFactor(water);
         growth = sunFactor * airFactor * waterFactor * 10;
         growthDamp = Mathf.SmoothDamp(growthDamp, growth, ref velocity, dampTime);
-        float meterAngle = 90 - 180 * (growthDamp / 10);
+        float meterAngle = -90 * (growthDamp / 10);
         growthMeter.eulerAngles = new Vector3(growthMeter.eulerAngles.x, growthMeter.eulerAngles.y, meterAngle);
 
     }
