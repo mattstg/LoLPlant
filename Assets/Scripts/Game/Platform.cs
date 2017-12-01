49fbe301ad9a8cb6b419080fbcdf169d9beb4851
mattstg <mattstger@gmail.com>
2017-12-01 03:18:36 -0500

    Platform structure setup
    platforms have defined edge objects, see tutorial scene for example,
    (May be replaced for something more user friendly)

--- Assets/Scripts/Game/Platform.cs
+++ Assets/Scripts/Game/Platform.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour {
 
+    public enum Side { Left = 0, Right = 1 }
     public bool hasAphid;
     public bool castsShadow;
 
@@ -13,12 +14,35 @@ public class Platform : MonoBehaviour {
     Vector2[] wp;
     public bool isStaticPlatform { get { return !moves; } }
     bool moves;
-    Vector2[] edges;
+    Vector2[] edgeOffsets;
+   
+    public Vector2 GetSide(Side side)
+    {
+        return edgeOffsets[(int)side] + new Vector2(transform.position.x, transform.position.y);
+    }
 
     public void Initialize()
     {
+        //Calculate edges
+        edgeOffsets = new Vector2[2];
+        Transform edgesParent = transform.Find("Edges");
+        if (!edgesParent || !edgesParent.GetChild(0) || !edgesParent.GetChild(1))
+        {
+            Debug.Log("Error! Platform: " + transform.name + " \"Edges\" transform missing, or missing two children objects for edges");
+            edgeOffsets[0] = edgeOffsets[1] = new Vector2();
+            return;
+        }
+        Transform child0, child1;
+        child0 = edgesParent.GetChild(0).transform;
+        child1 = edgesParent.GetChild(1).transform;
+        edgeOffsets[0] = (child0.position.x > child1.position.x) ? child0.position : child0.position;  //Left edge is stored in first offset array slot
+        edgeOffsets[1] = (child0.position.x <= child1.position.x) ? child0.position : child0.position;
+        edgeOffsets[0] = edgeOffsets[0] - new Vector2(transform.position.x, transform.position.y);   //Edges are turned into offset instead of hard values
+        edgeOffsets[1] = edgeOffsets[1] - new Vector2(transform.position.x, transform.position.y);
+        Destroy(edgesParent.gameObject);
+
         //Calculate Waypoints
-        Transform wpTransform = transform.Find("wp");
+        Transform wpTransform = transform.Find("WP");
         int numOfWPinWPTransform = (wpTransform != null) ?wpTransform.childCount:0;
         numOfWP = numOfWPinWPTransform + 1; //Includes its starting position as a waypoint
         //current point is always waypoint zero
@@ -29,11 +53,6 @@ public class Platform : MonoBehaviour {
         if (wpTransform != null)
             Destroy(wpTransform.gameObject);
         moves = (numOfWP > 1);
-
-        //Calculate edges
-        edges = new Vector2[2];
-        Transform edgesParent = transform.Find("edges");
-
     }
 
 	public void Refresh(float dt)
