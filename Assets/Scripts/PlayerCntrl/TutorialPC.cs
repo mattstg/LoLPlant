9d04ac98530462c2bd40caba5dc8d641c7474d87
mattstg <mattstger@gmail.com>
2017-12-13 21:36:50 -0500

    trigger for aphids in tutorial

--- Assets/Scripts/PlayerCntrl/TutorialPC.cs
+++ Assets/Scripts/PlayerCntrl/TutorialPC.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialPC : PlayerController {
-   
+    public override void OnCollisionEnter2D(Collision2D coli)
+    {
+        switch (coli.gameObject.tag)
+        {
+            case "Aphid":
+                TAEventManager.Instance.ReceiveActionTrigger("Aphid");
+                break;
+        }
+    }
 }
